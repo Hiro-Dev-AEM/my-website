802bf0980715896a28c8959b58562c65d7653067
himai <himai@adobe.com>
2023-11-17 15:13:07 +0900

    ok

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,21 +13,22 @@ import {
   loadCSS,
 } from './aem.js';
 const LCP_BLOCKS = []; // add your LCP blocks to the list
-
+// Define an execution context
+const pluginContext = {
+  getAllMetadata,
+  getMetadata,
+  loadCSS,
+  loadScript,
+  sampleRUM,
+  toCamelCase,
+  toClassName,
+};
 // Experiments
 const AUDIENCES = {
   mobile: () => window.innerWidth < 600,
   desktop: () => window.innerWidth >= 600,
   // define your custom audiences here as needed
 };
-
-window.hlx.plugins.add('experimentation', {
-  condition: () => getMetadata('experiment')
-    || Object.keys(getAllMetadata('campaign')).length
-    || Object.keys(getAllMetadata('audience')).length,
-  options: { audiences: AUDIENCES },
-  url: '/plugins/experimentation/src/index.js',
-});
 
 /**
  * Gets all the metadata elements that are in the given scope.
